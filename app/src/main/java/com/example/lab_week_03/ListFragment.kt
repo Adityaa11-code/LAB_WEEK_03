19b123935ebed25b5d8c6d5547e2b077f048ab34
Adityaa11-code <winstrek212@gmail.com>
2025-10-05 09:57:46 +0700

    git commit -m "Commit No. 2 – add coffee list & detail fragment"

---
+++ app/src/main/java/com/example/lab_week_03/ListFragment.kt
@@ -0,0 +1,47 @@
+package com.example.lab_week_03
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class ListFragment : Fragment(), View.OnClickListener {
+
+    private lateinit var coffeeListener: CoffeeListener
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is CoffeeListener) {
+            coffeeListener = context
+        } else {
+            throw RuntimeException("$context must implement CoffeeListener")
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val coffeeList = listOf(
+            R.id.affogato,
+            R.id.americano,
+            R.id.latte
+        )
+        coffeeList.forEach { id ->
+            view.findViewById<TextView>(id).setOnClickListener(this)
+        }
+    }
+
+    override fun onClick(v: View) {
+        coffeeListener.onSelected(v.id)
+    }
+}
